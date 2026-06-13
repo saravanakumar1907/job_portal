627fc1fa6c0b6bdea546423b0d70dcbc3764fdd0
Saravanakumar <135038409+saravanakumar1907@users.noreply.github.com>
2026-06-13 19:38:17 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -41,3 +41,7 @@ def apply():
         return "Application submitted successfully!"
     return render_template('form.html')
 
+    
+    if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
